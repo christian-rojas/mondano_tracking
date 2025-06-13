615269fc2182b0bde97a2a80028369d8edb6bc13
christian-rojas <christian.ici17@gmail.com>
2025-06-13 15:11:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route to serve index.html on "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
